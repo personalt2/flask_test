42cbd65d0650e32dd4d46c5ff3ff94b6bf9b8a24
rich <rich>
2021-02-13 12:17:29 -0500

    add list of modules

---
+++ pip_modules.sh
@@ -0,0 +1,12 @@
+
+pip install pymysql    
+pip install schedule
+pip install python-dateutil
+pip install sentry_sdk    
+pip install python-dateutil
+pip install mws          
+pip install httplib2
+pip install maya    
+
+
+
